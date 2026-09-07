c77014c6d2a7fcafca6e4658fd70f829d2efb97f
matkow333-hash <matkow333@icloud.com>
2026-09-07 12:36:19 +0200

    Usuniecie pliku testowego

--- _lt.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright
-try:
-    with sync_playwright() as p:
-        b = p.chromium.launch(headless=True)
-        b.close()
-    print("LAUNCH OK")
-except Exception as e:
-    print("LAUNCH BLAD:", str(e).split("Call log")[0].strip()[:120])
